11fbdafbf2dcb26cef9e42fec74bdcba5726602a
Menjith P <70083973+menjithp@users.noreply.github.com>
2023-10-22 12:59:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.get("/events",async(req,res)=>{
                   requestId: 'coding-calendar-demo'
                 }
               },
-            attendees:[{email:"menjithchandra2000@gmail.com"},{email:"biohackering@gmail.com"}]             
+            attendees:[]             
         },
       });
       
